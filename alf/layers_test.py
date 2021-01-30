6b5545385dfe708009b222b49392fac1ad2688ac
Haichao Zhang <hczhang1@gmail.com>
2021-01-29 19:22:00 -0800

    Test the max difference

--- alf/layers_test.py
+++ alf/layers_test.py
@@ -560,6 +560,12 @@ class LayersTest(parameterized.TestCase, alf.test.TestCase):
         self.assertTensorClose(z[0:64], z[64:128], epsilon=1e-10)
         self.assertTensorClose(z[0:64], z[128:192], epsilon=1e-10)
         self.assertTensorClose(z[0:64], z[192:256], epsilon=1e-10)
+
+        print("======12345")
+        print((z[0:64] - z[64:128]).abs().max())
+        print((z[0:64] - z[128:192]).abs().max())
+        print((z[0:64] - z[192:256]).abs().max())
+
         self.assertTrue((z[0:8] != z[8:16]).all())
         self.assertTrue((z[0:8] != z[16:24]).all())
         self.assertTrue((z[0:8] != z[24:32]).all())
